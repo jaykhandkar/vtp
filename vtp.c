c36c21b6ad31adbe412c62e730178742eb6494bb
Jay Khandkar <jaykhandkar2002@gmail.com>
2021-01-24 21:47:55 +0530

    added ioctl operation

--- vtp.c
+++ vtp.c
@@ -20,10 +20,35 @@ int vtp_major = 0;		/*we shall allocate dynamically*/
 int vtp_minor = 0;
 int nr_vtp_devs = 1;		/*just one device*/
 
+struct cdev *vtp_cdev;		/*our character device*/
+
+
+/*
+ * Function:  vtp_ioctl 
+ * --------------------
+ * converts the user-space virtual address in arg to it's corresponding physical
+ * address by walking the current process' page tables.
+ *
+ *  arg: the user-space virtual address to be converted
+ *
+ *  returns: -1 for any failure, zero otherwise
+ */
+long vtp_ioctl(struct file *filp, unsigned int cmd, unsigned long arg)
+{
+	printk(KERN_NOTICE"ioctl has been called on the char device!\n");
+	return 0;
+}
+
+const struct file_operations vtp_fops = {
+   .owner =    THIS_MODULE,
+   .unlocked_ioctl = vtp_ioctl,		/*we'll only use ioctl*/
+};
+
 int vtp_init(void)
 {
-	int result;
+	int result, err;
 	dev_t dev = 0;
+	
 	
 	result = alloc_chrdev_region(&dev, vtp_minor, nr_vtp_devs,
 				     "vtp");
@@ -33,6 +58,14 @@ int vtp_init(void)
 		printk(KERN_WARNING "vtp: can't allocate device number\n");
 		return result;
 	}
+	
+	vtp_cdev = cdev_alloc();
+	vtp_cdev->ops = &vtp_fops;
+	vtp_cdev->owner = THIS_MODULE;
+	err = cdev_add(vtp_cdev, dev, 1);
+	if (err)
+		printk(KERN_NOTICE "couldn't add cdev: error %d", err);
+	
 	return 0;
 }
 
@@ -41,5 +74,8 @@ void vtp_exit(void)
 	dev_t dev = MKDEV(vtp_major, vtp_minor);
 	
 	unregister_chrdev_region(dev, nr_vtp_devs);
+	cdev_del(vtp_cdev);
 }
+module_init(vtp_init);
+module_exit(vtp_exit);
 
